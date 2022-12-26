839c4405ee324311872099cf3879dd3d49089f47
RedwanAhmed1 <redwans88@gmail.com>
2022-12-25 20:28:19 -0500

    added family message

--- src/main/java/LearnMaven/MavenJava/App.java
+++ src/main/java/LearnMaven/MavenJava/App.java
@@ -10,5 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello me!" );
+        System.out.println( "Hello wife!" );
+        System.out.println( "Hello kiddo!" );
     }
 }
